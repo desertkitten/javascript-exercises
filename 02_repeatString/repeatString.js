5db8a398a2af98681f266fa40e575905cd4ddec5
desertkitten <56772806+desertkitten@users.noreply.github.com>
2024-07-02 16:22:37 +0000

    change code

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(string, num) {
     let result = '';
 
-    for (i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         result += string;
     }
     return result;
