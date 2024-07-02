111f291adecef649473e5a801e0e7995c6751905
desertkitten <56772806+desertkitten@users.noreply.github.com>
2024-07-02 18:13:12 +0000

    sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    let min = Math.min(start, end);
+    let max = Math.max(start, end);
 
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
+console.log(sumAll(1, 4)); // Output: 10
 // Do not edit below this line
 module.exports = sumAll;
