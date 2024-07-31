fc2c5449417454899e364d8c41fea45012e71e03
desertkitten <56772806+desertkitten@users.noreply.github.com>
2024-07-31 16:28:07 +0000

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    if (num === 1) {
+        return num1;
+    } else if (num === 2) {
+        return num2;
+    } else {
+        for (let i = 3; i <= num; i++) {
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+        }
+        return num2;
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = fibonacci;
