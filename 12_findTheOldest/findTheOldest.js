b628b869fee0459b2d73502a9f3452458fc20ef7
desertkitten <56772806+desertkitten@users.noreply.github.com>
2024-08-01 16:21:31 +0000

    oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(birth, death) {
+    let maxAge = 0;
+    let oldestPersonIndex = -1;
 
+    for (let i = 0; i < birth.length; i++) {
+        let age;
+        if (death[i] !== undefined) {
+            age = death[i] - birth[i];
+        } else {
+            age = new Date().getFullYear() - birth[i];
+        }
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldestPersonIndex = i;
+        }
+    }
+
+    return oldestPersonIndex;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
